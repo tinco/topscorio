3df040b26b4a985670f19bc3b396332d9d7c5eb8
Tinco Andringa <mail@tinco.nl>
2021-04-21 23:46:05 +0200

    sending e-mails with sendgrid works

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,6 +1,7 @@
 import express from "express"
 import dotenv from "dotenv"
 import redis from "redis"
+import mail from "@sendgrid/mail"
 
 dotenv.config()
 
@@ -14,6 +15,24 @@ authStore.on("error", (error) => {
 })
 
 authStore.setex("test", 600, "123", redis.print)
+
+mail.setApiKey(process.env.SENDGRID_API_KEY)
+
+const msg = {
+  to: 'mail@tinco.nl',
+  from: 'mail@tinco.nl', // Change to your verified sender
+  subject: 'Sending with SendGrid is Fun',
+  text: 'and easy to do anywhere, even with Node.js',
+  html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+}
+
+mail.send(msg)
+  .then(() => {
+    console.log('Email sent')
+  })
+  .catch((error) => {
+    console.error(error)
+  })
 
 // define a route handler for the default home page
 app.get( "/", ( req, res ) => {
