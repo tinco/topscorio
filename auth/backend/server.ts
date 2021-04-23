13e254f5ab9e4baafad0cf9cf4c498c851ca0e17
Tinco Andringa <mail@tinco.nl>
2021-04-24 01:54:25 +0200

    start on token system

--- auth/backend/server.ts
+++ auth/backend/server.ts
@@ -1,39 +1,23 @@
 import express from "express"
 import dotenv from "dotenv"
-import redis from "redis"
+import Redis from "ioredis"
 import mail from "@sendgrid/mail"
 import ws from "ws"
+import cryptoRandomString from 'crypto-random-string'
 
 dotenv.config()
 
 const app = express()
 const port = process.env.SERVER_PORT
 
-const authStore = redis.createClient(process.env.AUTH_REDIS)
+const authStore = new Redis(process.env.AUTH_REDIS)
 
 authStore.on("error", (error) => {
     console.error("REDIS_ERROR:", error)
 })
 
-authStore.setex("test", 600, "123", redis.print)
-
 mail.setApiKey(process.env.SENDGRID_API_KEY)
 
-// const msg = {
-//   to: 'mail@tinco.nl',
-//   from: 'mail@tinco.nl', // Change to your verified sender
-//   subject: 'Sending with SendGrid is Fun',
-//   text: 'and easy to do anywhere, even with Node.js',
-//   html: '<strong>and easy to do anywhere, even with Node.js</strong>',
-// }
-
-// mail.send(msg)
-//   .then(() => {
-//     console.log('Email sent')
-//   })
-//   .catch((error) => {
-//     console.error(error)
-//   })
 
 const authWS = new ws.Server({ noServer: true })
 authWS.on('connection', (socket, _head) => {
@@ -45,12 +29,35 @@ app.get( "/", ( req, res ) => {
     res.send( "Hello world!" )
 } )
 
-app.post( "/authenticate", ( req, res) => {
-    // we generate a token and save it in redis
+const handleAuthReq = async (email: string) : Promise<any> => {
     // we start listening on a websocket for a confirmation
     // we start listening internally for a confirmation
     // then we send an e-mail
     // when the link in the e-mail is clicked
+    const token = cryptoRandomString({length: 10})
+    const redisResult = await authStore.setex(token, 600, JSON.stringify({
+        email: email
+    }))
+
+    const mailResult = await mail.send({
+        to: 'mail@tinco.nl',
+        from: 'mail@tinco.nl', // Change to your verified sender
+        subject: 'Sending with SendGrid is Fun',
+        text: 'and easy to do anywhere, even with Node.js',
+        html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+    })
+
+    return {
+        tokenResult: redisResult,
+        mailResult: mailResult
+    }
+}
+
+app.post( "/authenticate", async ( req, res) => {
+    // we generate a token and save it in redis
+
+
+    // res.json({ result: redisResult })
 })
 
 // start the Express server
